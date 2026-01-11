7ff8dc112b740fd29419b7cb559cbc0d3a099f61
arpitkasaudhan <kasaudhanarpit37@gmail.com>
2026-01-11 16:29:25 +0530

    learning commits setting of ts

---
+++ typescript-learning-guide/self/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function myname(name) {
+    return `my name is ${name}`;
+}
+console.log(myname('aman'));
+console.log(myname('sumit'));
+//# sourceMappingURL=index.js.map
---
+++ typescript-learning-guide/self/src/index.ts
@@ -0,0 +1,6 @@
+function myname(name:string):string{
+    return `my name is ${name}`
+}
+console.log(myname('aman'));
+console.log(myname('sumit'));
+
