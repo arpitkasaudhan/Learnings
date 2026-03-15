b95690d11ca9e4034d1a1cdceb653d395d87c095
arpitkasaudhan <kasaudhanarpit37@gmail.com>
2026-03-15 20:21:16 +0530

    typscript lectures

---
+++ onegotypescript/index.js
@@ -0,0 +1,9 @@
+var named = 'offkm';
+var wallet;
+console.log(typeof wallet);
+var halchall = function (name, parents) {
+    if (parents === void 0) { parents = 'mummy'; }
+    console.log("aut ".concat(name, " bhai ").concat(parents, " kaisi hai "));
+    return 'always';
+};
+halchall('Arpit', 'PAPA');
---
+++ onegotypescript/index.ts
@@ -0,0 +1,16 @@
+const named:string= 'offkm';
+
+let wallet:'paisa';
+
+console.log(typeof wallet);
+
+let halchall=function(name:string,parents:string='mummy'):string{
+    console.log(`aut ${name} bhai ${parents} kaisi hai `);
+    return 'always'
+}
+
+halchall('Arpit','PAPA');
+
+
+
+
